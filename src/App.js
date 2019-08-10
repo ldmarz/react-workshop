0aaa2418d0de7b0cddfb4b17186856434bef1f1d
lenin <lenin.martinez@mercadolibre.cl>
2019-08-10 10:31:04 -0400

    adding button with elemen reference

--- src/App.js
+++ src/App.js
@@ -3,17 +3,43 @@ import { hot } from 'react-hot-loader';
 import './App.scss';
 import TodayList from './components/TodayList';
 import TomorrowList from './components/TomorrowList';
+import ErrorBoundary from "./components/ErrorBoundary";
+import Button from "./components/Button";
 
 class App extends Component {
   constructor(props) {
     super(props);
+
+    this.buttonRef = (elem) => {this.buttonRef = elem; }
+    this.rollToButton = this.rollToButton.bind(this);
+  }
+
+  rollToButton() {
+      if (this.buttonRef) {
+          this.buttonRef.scrollIntoView({
+              behavior: "smooth",
+              block: "start"
+          })
+      }
   }
 
   render() {
     return (
       <div className="App" id="app">
-        <TodayList title="Lista para hoy"/>
-        <TomorrowList title={"Lista para mañana"}/>
+          <ErrorBoundary>
+              <TodayList title="Lista para hoy"/>
+          </ErrorBoundary>
+
+          <ErrorBoundary>
+              <TomorrowList title={"Lista para mañana"}/>
+          </ErrorBoundary>
+
+
+          <Button
+            buttonRef={this.buttonRef}
+            onClick={this.rollToButton}
+            text={"Scroll to button"}
+          />
       </div>
     );
   }
--- src/components/TodayList.js
+++ src/components/TodayList.js
@@ -1,31 +1,4 @@
 import withTodoListService from "./withTodoListService";
 import List from "./List";
 
-export default withTodoListService(List, "today");
-
-/*class TodayList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      list: todoListService.getList('today')
-    };
-    this.onChangeHandler = this.onChangeHandler.bind(this);
-  }
-
-  onChangeHandler(e, id) {
-    e.preventDefault();
-    todoListService.updateItem('today', id);
-    setTimeout(() => {
-      this.setState({ list: todoListService.getList('today') });
-    }, 10);
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Today to-do list</h1>
-        <List list={this.state.list} handler={this.onChangeHandler} />
-      </div>
-    );
-  }
-}*/
+export default withTodoListService(List, "today")
